7fa7bba9d4b0942de3fdb6ce3dbfb18f06d12051
AurelienAllenic <aurelien.allenic@gmail.com>
2025-01-27 10:54:15 +0100

    (indent) removed useless comments from App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
-import TodoDetail from './components/todo/TodoDetail'; // La nouvelle page pour afficher les détails d'un todo
+import TodoDetail from './components/todo/TodoDetail';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/todos/:id" element={<TodoDetail />} /> {/* Page de détails du Todo */}
+        <Route path="/todos/:id" element={<TodoDetail />} />
       </Routes>
     </Router>
   );
